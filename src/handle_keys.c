adeb06a3f109fcecf55aca9cbd87f9f74dcfdfc6
Luis Martín <luismartingarcia4@gmail.com>
2025-06-11 23:20:45 +0200

    a y d listo

--- src/handle_keys.c
+++ src/handle_keys.c
@@ -6,7 +6,7 @@
 /*   By: lumartin <lumartin@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 17:43:24 by aldara            #+#    #+#             */
-/*   Updated: 2025/06/11 17:44:51 by lumartin         ###   ########.fr       */
+/*   Updated: 2025/06/11 23:19:19 by lumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,10 @@ void	handle_key_a(t_game *game)
 {
 	if (mlx_is_key_down(game->mlx, MLX_KEY_A))
 	{
-		if (game->map->map[(int)(game->pos_y)][(int)(game->pos_x - game->dir_y
+		if (game->map->map[(int)(game->pos_y)][(int)(game->pos_x + game->dir_y
 				* game->move_speed)] != '1')
 			game->pos_x += game->dir_y * game->move_speed;
-		if (game->map->map[(int)(game->pos_y - game->dir_y
+		if (game->map->map[(int)(game->pos_y - game->dir_x
 				* game->move_speed)][(int)(game->pos_x)] != '1')
 			game->pos_y -= game->dir_x * game->move_speed;
 	}
@@ -58,7 +58,7 @@ void	handle_key_d(t_game *game)
 		if (game->map->map[(int)(game->pos_y)][(int)(game->pos_x - game->dir_y
 				* game->move_speed)] != '1')
 			game->pos_x -= game->dir_y * game->move_speed;
-		if (game->map->map[(int)(game->pos_y - game->dir_y
+		if (game->map->map[(int)(game->pos_y + game->dir_x
 				* game->move_speed)][(int)(game->pos_x)] != '1')
 			game->pos_y += game->dir_x * game->move_speed;
 	}
